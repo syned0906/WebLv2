6ee55f380d10ab0835771c8c498ba950a4f2fcb4
syned0906 <ivancestar525@gmail.com>
2026-04-09 04:09:56 +0200

    Inicijalni commit

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-
+app.use(express.static('public'));
 
 
 
