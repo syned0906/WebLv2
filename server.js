c748b730394d10b6521cdb0b8e780e18f636de79
syned0906 <ivancestar525@gmail.com>
2026-04-09 04:07:21 +0200

    Inicijalni commit

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-res.send('Pozdrav sa Railway servera!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+
+
+
+
+
 app.listen(PORT, () => {
 console.log('Server pokrenut na portu ${PORT}');
 });
